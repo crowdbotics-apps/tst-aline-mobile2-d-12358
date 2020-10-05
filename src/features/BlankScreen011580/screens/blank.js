db8982bc228011052b81cce8ecbdf90a77ec8439
Crowdbotics <team@crowdbotics.com>
2020-10-05 15:14:47 +0000

    Update components

--- src/features/BlankScreen011580/screens/blank.js
+++ src/features/BlankScreen011580/screens/blank.js
@@ -12,22 +12,26 @@ import {
   Datepicker
 } from "react-native-ui-kitten"
 import Slider from "@react-native-community/slider"
-
 import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 import { connect } from "react-redux"
-export class __Blank extends React.Component {
+export class _Blank extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
   }
   state = {}
   render = () => (
     <View style={styles.View_1}>
-      <Text style={styles.Text_3}>Sample text content</Text>
+      <Button
+        title="Press me!"
+        color="#3366FF"
+        style={styles.Button_3}
+        onPress={() => alert("Pressed!")}
+      />
     </View>
   )
 }
 
-_Blank = withStyles(__Blank, theme => ({
+Blank = withStyles(_Blank, theme => ({
   container: { backgroundColor: theme["color-basic-100"] }
 }))
 
@@ -35,4 +39,4 @@ function mapStateToProps(state) {
   return { state: state.apiReducer }
 }
 const actionCreators = {}
-export default connect(mapStateToProps, actionCreators)(_Blank)
+export default connect(mapStateToProps, actionCreators)(Blank)
